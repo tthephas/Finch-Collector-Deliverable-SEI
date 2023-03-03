78c50e32890443c70d3f7c25e8b1424ea923ad77
thepjos <josh.thephasdin@gmail.com>
2023-03-03 10:31:22 -0600

    delete done

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Finch
 
@@ -33,5 +33,15 @@ def finches_detail(request, finch_id):
 class FinchCreate(CreateView):
   model = Finch
   fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  
+  fields = ['color', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
+
 
 
